790d54f5834661c2ab9f3cde2652a9cbf77a54ec
Desttro <bozstvi-mluvil0a@icloud.com>
2026-02-24 18:54:31 +0100

    fix report card

--- src/features/reports/ReportCard.tsx
+++ src/features/reports/ReportCard.tsx
@@ -46,7 +46,7 @@ export const ReportCard = memo(({ report, isExpired = false }: ReportCardProps) 
   return (
     <YStack
       onPress={handlePress}
-      flex={1}
+      grow={1}
       p="$4"
       rounded="$5"
       borderWidth={1}
